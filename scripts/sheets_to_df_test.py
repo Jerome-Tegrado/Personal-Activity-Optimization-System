0ebe91aadc9f1fee27b1e548dcaacbfad7a82437
Jerome-Tegrado <tegradojerome@gmail.com>
2026-01-28 18:35:17 +0800

    chore(scripts): fix imports for sheets df test

--- scripts/sheets_to_df_test.py
+++ scripts/sheets_to_df_test.py
@@ -1,4 +1,4 @@
-from src.paos.ingest.sheets_ingest import SheetsConfig, read_daily_log_from_sheets
+from paos.ingest.sheets_ingest import SheetsConfig, read_daily_log_from_sheets
 
 cfg = SheetsConfig(
     spreadsheet_id="1Ma4p5j8CW4EO98a2Xx6HBkUjTrt46o6-xL8ZpLxmk9U",
